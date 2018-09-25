a37a97a0843dca57837776bf65328ba58ab52781
Albe96XDA <albe96xda@gmail.com>
2018-09-25 23:21:49 +0530

    Revert "add always\never enforce"
    This reverts commit f8e4f5aa9db3bb9529eadb9547256537a3e3fe49.

--- security/selinux_n/ss/services.c
+++ security/selinux_n/ss/services.c
@@ -774,7 +774,8 @@ out:
 	kfree(t);
 
 // [ SEC_SELINUX_PORTING_COMMON
-#ifdef CONFIG_SECURITY_SELINUX_ALWAYS_ENFORCE
+#ifdef CONFIG_ALWAYS_ENFORCE
+#if !defined(CONFIG_RKP_KDP)
 	selinux_enforcing = 1;
 #endif
 #endif
@@ -1544,7 +1545,8 @@ out:
 	kfree(n);
 
 // [ SEC_SELINUX_PORTING_COMMON
-#ifdef CONFIG_SECURITY_SELINUX_ALWAYS_ENFORCE
+#ifdef CONFIG_ALWAYS_ENFORCE
+#if !defined(CONFIG_RKP_KDP)
 	selinux_enforcing = 1;
 #endif
 #endif
@@ -1841,7 +1843,8 @@ static inline int convert_context_handle_invalid_context(struct context *context
 	u32 len;
 
 // [ SEC_SELINUX_PORTING_COMMON
-#ifdef CONFIG_SECURITY_SELINUX_ALWAYS_ENFORCE
+#ifdef CONFIG_ALWAYS_ENFORCE
+#if !defined(CONFIG_RKP_KDP)
 	selinux_enforcing = 1;
 #endif
 #endif
